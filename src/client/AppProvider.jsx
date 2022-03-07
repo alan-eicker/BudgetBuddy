adae82c3818cf84609ddd65e6e326bff9bb10ded
Alan Eicker <alaneicker@gmail.com>
2022-03-07 16:14:36 -0600

    Adds logic to verify token and redirect

--- src/client/AppProvider.jsx
+++ src/client/AppProvider.jsx
@@ -1,14 +1,39 @@
-import React, { useContext, createContext, useState } from 'react';
+import React, { useContext, createContext, useState, useEffect } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
+import { gql, useQuery } from '@apollo/client';
 import PropTypes from 'prop-types';
+
+// const VERIFY_TOKEN = gql``;
 
 const AppContext = createContext({});
 
 export const useAppContext = () => useContext(AppContext);
 
 const AppProvider = ({ children }) => {
+  const { pathname } = useLocation();
+  const history = useHistory();
+
   const [showLoader, setShowLoader] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [hasValidToken, setHasValidToken] = useState(true);
+
   const budgetLimitPercentage = 80;
+
+  const isProtectedRoute = pathname.match(/dashboard|expense-group/);
+
+  // Run useQuery with skip option if isProtectedRoute === false
+  // Otherwise, verify token and return `true` or `false` with response;
+  // const { loading, error, data } = useQuery(VERIFY_TOKEN, {
+  //   skip: !isProtectedRoute
+  // });
+
+  useEffect(() => {
+    if (!hasValidToken) {
+      history.push('/');
+    } else {
+      history.push(pathname);
+    }
+  }, [hasValidToken]);
 
   return (
     <AppContext.Provider
