7a44aa9be5cacd39e52ff16662d7705d133d6ea4
Alan Eicker <alaneicker@gmail.com>
2022-03-15 20:59:40 -0500

    Update ExpenseGroupLayout.jsx

--- src/client/components/ExpenseGroupLayout/ExpenseGroupLayout.jsx
+++ src/client/components/ExpenseGroupLayout/ExpenseGroupLayout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { ButtonControls, Button, Statistic, Hint, Alert } from '@atomikui/core';
@@ -26,83 +26,95 @@ const ExpenseGroupLayout = ({
   budgetLimitPercentage,
   startDate,
   endDate,
-}) => (
-  <div className="expense-group">
-    <div className="expense-group__head">
-      <h1 className="expense-group__title">
-        {formatDate(startDate)} - {formatDate(endDate)}
-        <span>Total Budget: ${formatNumber(totalBudget)}</span>
-      </h1>
-      <ButtonControls className="expense-group__controls">
-        <Link title="go back" aria-label="go back" to="/expense-groups">
-          <Icon icon={faArrowLeft} size="lg" />
-        </Link>
-        <Link
-          title="edit expense"
-          aria-label="edit expense"
-          to={`/expense-groups/edit/${id}`}
-        >
-          <Icon icon={faPen} />
-        </Link>
-        <Button
-          theme="red"
-          size="md"
-          shape="pill"
-          title="delete group"
-          aria-label="delete group"
-          onClick={() => {}}
-        >
-          <Icon icon={faTimes} />
-        </Button>
-      </ButtonControls>
-    </div>
-    <div className="expense-group__body">
-      <div className="expense-group__expenses">
-        {overdueExpenses > 0 && (
-          <Alert theme="error" className="margin-bottom-16">
-            You have {overdueExpenses} unpaid overdue expense{''}
-            {overdueExpenses > 1 && 's'}.
-          </Alert>
-        )}
-        <ExpenseList expenses={expenses} />
+}) => {
+  const [showAlert, setShowAlert] = useState(false);
+
+  useEffect(() => {
+    setShowAlert(overdueExpenses > 0);
+  }, [overdueExpenses]);
+
+  return (
+    <div className="expense-group">
+      <div className="expense-group__head">
+        <h1 className="expense-group__title">
+          {formatDate(startDate)} - {formatDate(endDate)}
+          <span>Total Budget: ${formatNumber(totalBudget)}</span>
+        </h1>
+        <ButtonControls className="expense-group__controls">
+          <Link title="go back" aria-label="go back" to="/expense-groups">
+            <Icon icon={faArrowLeft} size="lg" />
+          </Link>
+          <Link
+            title="edit expense"
+            aria-label="edit expense"
+            to={`/expense-groups/edit/${id}`}
+          >
+            <Icon icon={faPen} />
+          </Link>
+          <Button
+            theme="red"
+            size="md"
+            shape="pill"
+            title="delete group"
+            aria-label="delete group"
+            onClick={() => {}}
+          >
+            <Icon icon={faTimes} />
+          </Button>
+        </ButtonControls>
       </div>
-      <div className="expense-group__summary">
-        <h2 className="text-size-20 margin-bottom-20">Spending Snapshot</h2>
-        <Statistic
-          theme={isAlmostOverBudget ? 'red' : 'green'}
-          value={totalBalance}
-          label="Total Balance"
-          size="md"
-          topLabel
-        />
-        {isAlmostOverBudget && (
-          <div className="expense-group__budget-warning">
-            <Icon icon={faExclamationTriangle} />
-            <Hint type="error">
-              Your total balance is greater than {budgetLimitPercentage}% of
-              this month&apos;s budget.
-            </Hint>
-          </div>
-        )}
-        <Statistic
-          className="margin-top-20"
-          value={unpaidBalance}
-          label="Unpaid Balance"
-          size="md"
-          topLabel
-        />
-        <Statistic
-          {...(isOverBudget && { theme: 'red' })}
-          className="margin-top-20"
-          value={leftOverBalance}
-          label="Left Over Balance"
-          size="md"
-          topLabel
-        />
+      <div className="expense-group__body">
+        <div className="expense-group__expenses">
+          {showAlert && (
+            <Alert
+              theme="error"
+              className="margin-bottom-16"
+              onClose={() => setShowAlert(false)}
+            >
+              You have {overdueExpenses} unpaid overdue expense{''}
+              {overdueExpenses > 1 && 's'}.
+            </Alert>
+          )}
+          <ExpenseList expenses={expenses} />
+        </div>
+        <div className="expense-group__summary">
+          <h2 className="text-size-20 margin-bottom-20">Spending Snapshot</h2>
+          <Statistic
+            theme={isAlmostOverBudget ? 'red' : 'green'}
+            value={totalBalance}
+            label="Total Balance"
+            size="md"
+            topLabel
+          />
+          {isAlmostOverBudget && (
+            <div className="expense-group__budget-warning">
+              <Icon icon={faExclamationTriangle} />
+              <Hint type="error">
+                Your total balance is greater than {budgetLimitPercentage}% of
+                this month&apos;s budget.
+              </Hint>
+            </div>
+          )}
+          <Statistic
+            className="margin-top-20"
+            value={unpaidBalance}
+            label="Unpaid Balance"
+            size="md"
+            topLabel
+          />
+          <Statistic
+            {...(isOverBudget && { theme: 'red' })}
+            className="margin-top-20"
+            value={leftOverBalance}
+            label="Left Over Balance"
+            size="md"
+            topLabel
+          />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 ExpenseGroupLayout.propTypes = {
   id: PropTypes.string,
