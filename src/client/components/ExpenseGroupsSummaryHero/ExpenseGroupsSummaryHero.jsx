d8e29e353908a24ca56bd9da2bf155841d46b312
Alan Eicker <alaneicker@gmail.com>
2022-04-04 15:15:48 -0500

    masthead icon styles

--- src/client/components/ExpenseGroupsSummaryHero/ExpenseGroupsSummaryHero.jsx
+++ src/client/components/ExpenseGroupsSummaryHero/ExpenseGroupsSummaryHero.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Flex } from '@atomikui/core';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import {
   faChevronRight,
