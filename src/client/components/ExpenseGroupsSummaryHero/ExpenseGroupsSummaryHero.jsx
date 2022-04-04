985e6a30ec7100df037362def8495a9943fb7e1c
Alan Eicker <alaneicker@gmail.com>
2022-04-04 13:57:21 -0500

    Expense Group Hero styles

--- src/client/components/ExpenseGroupsSummaryHero/ExpenseGroupsSummaryHero.jsx
+++ src/client/components/ExpenseGroupsSummaryHero/ExpenseGroupsSummaryHero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Flex } from '@atomikui/core';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import {
   faChevronRight,
@@ -25,7 +26,27 @@ const ExpenseGroupsSummaryHero = ({ _id, startDate, endDate, expenses }) => {
     <Link className="expense-group-summary-hero" to={`/expense-groups/${_id}`}>
       <div className="expense-group-summary-hero__body">
         <h1 className="expense-group-summary-hero__title">
-          Your Current Expenses
+          <svg
+            className="expense-group-summary-hero__title__icon"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 50 50"
+          >
+            <defs />
+            <title>Сalendar</title>
+            <g id="Layer_2" data-name="Layer 2">
+              <g id="Сalendar">
+                <path d="M41.92 5H40V2.5a2.5 2.5 0 0 0-5 0V5H15V2.5a2.5 2.5 0 0 0-5 0V5H8.08A8.08 8.08 0 0 0 0 13.08v28.84A8.08 8.08 0 0 0 8.08 50h33.84A8.08 8.08 0 0 0 50 41.92V13.08A8.08 8.08 0 0 0 41.92 5zM45 41.92A3.08 3.08 0 0 1 41.92 45H8.08A3.08 3.08 0 0 1 5 41.92V13.08A3.08 3.08 0 0 1 8.08 10H10v2.5a2.5 2.5 0 0 0 5 0V10h20v2.5a2.5 2.5 0 0 0 5 0V10h1.92A3.08 3.08 0 0 1 45 13.08z" />
+                <circle cx="25" cy="25.5" r="2.5" />
+                <circle cx="25" cy="34.5" r="2.5" />
+                <circle cx="16" cy="34.5" r="2.5" />
+                <g>
+                  <circle className="is-today" cx="34" cy="25.5" r="2.5" />
+                  <circle className="cls-2" cx="34" cy="34.5" r="2.5" />
+                </g>
+              </g>
+            </g>
+          </svg>
+          <span>Your Current Expenses</span>
         </h1>
         <h2 className="expense-group-summary-hero__subtitle">{title}</h2>
         <div className="expense-group-summary-hero__balance">
